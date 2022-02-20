77ab4b7de2d9a2fd76721df21335e0a482da39a7
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-21 01:11:20 +0200

    adding post and get signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'sessions#home'
+  root 'sessions#login'
 
   #users routes
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
   #sessions routes
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#create'
+  get '/auth/login', to: 'sessions#login'
+  post '/auth/login', to: 'sessions#create'
+
+  get '/signup', to: 'users#new' # getting url
+  post '/signup', to: 'users#create' # adding user to db
+
   post '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
 
