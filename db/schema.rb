969d78a4c99c27034130b3f14e68dd5cacde49fa
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-21 03:39:37 +0200

    Making each user unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_124500) do
+ActiveRecord::Schema.define(version: 2022_02_20_235148) do
 
   create_table "todo_items", force: :cascade do |t|
     t.text "description"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_02_20_124500) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "todo_items", "todo_lists"
