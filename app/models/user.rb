2628379a28284c329e2dd1273f49e5166cc0954b
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-21 03:40:57 +0200

    Making users unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :todo_lists, :dependent => :destroy
   has_secure_password
   validates :username, presence: true
+  validates :username, uniqueness: {message: "This username has already been taken"}
 end
