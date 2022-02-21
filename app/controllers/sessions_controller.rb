9ca129fd56ef4844b031ab7d9dd3dde2a8d94148
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-21 03:40:41 +0200

    Login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,28 @@
 class SessionsController < ApplicationController
-  def create
-    @user = User.find_by(username: params[:username])
+  def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def login
+    user = User.find_by(username: params[:username])
 
     #authenticate user credentials
-    if !!@user && @user.authenticate(params[:password])
+    if user.present? && user.authenticate(params[:password])
 
       #set session and redirect on success
-      session[:user_id] = @user.id
-      redirect_to user_path
+      session[:user_id] = user.id
+      redirect_to todo_lists_path
     else
       #error massage fail
-      message = " Something went wrong! Make sure your username and password are correct "
-      redirect_to login_path, notice: message
-
-
+      flash[:alert] = "Invalid credentials"
+      render :login
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to auth_login_path, notice: "Logged out"
   end
 end
