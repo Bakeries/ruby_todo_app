065973e7aabf0a9bd04ceed59c8f9a2b1ebfa8bd
Constantinos Constantinides <bakeries.g9@gmail.com>
2022-02-21 03:37:01 +0200

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,31 @@
 Rails.application.routes.draw do
 
-  root 'sessions#login'
+  resources :todo_lists do
+    resources :todo_items
+  end
+  namespace :todo_list do
+    resources :todo_items
+  end
+
+  resources :users
+  root 'sessions#index'
 
   #users routes
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
   #sessions routes
-  get '/auth/login', to: 'sessions#login'
-  post '/auth/login', to: 'sessions#create'
+  get '/auth/login', to: 'sessions#index'
+  post '/auth/login', to: 'sessions#login'
 
   get '/signup', to: 'users#new' # getting url
-  post '/signup', to: 'users#create' # adding user to db
+  post '/signup', to: 'users#create' # save user to db
 
-  post '/logout', to: 'sessions#destroy'
-  get '/logout', to: 'sessions#destroy'
+  post '/auth/logout', to: 'sessions#destroy'
+  get '/auth/logout', to: 'sessions#destroy'
 
+  # /todos get and post
+  # get '/todos', to: 'todo_lists#index'
+  # post '/todos/new', to: 'todo_lists#new'
+
+  # /todos/:id get, put, delete
 end
